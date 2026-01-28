ed5c602f671f9c988cf32aa873a07a21de471efa
Gabriel Duarte <gabrielduartesilvan@gmail.com>
2026-01-27 23:35:31 -0300

    First Commit

---
+++ src/main/java/com/chat/livechat/domain/ChatInput.java
@@ -0,0 +1,7 @@
+package com.chat.livechat.domain;
+
+public record ChatInput(
+        String user,
+        String message
+) {
+}
---
+++ src/main/java/com/chat/livechat/domain/ChatOutput.java
@@ -0,0 +1,6 @@
+package com.chat.livechat.domain;
+
+public record ChatOutput(
+        String content
+) {
+}
